8639cab7f985e93c68628b1786701a86a0c69b8f
Omkar Javadwar <43721555+omkar-javadwar@users.noreply.github.com>
2021-06-22 13:53:26 +0530

    admin_day_6

--- admin/controllers/adminControllers.js
+++ admin/controllers/adminControllers.js
@@ -17,19 +17,16 @@ exports.addAdmin = async (req, res) => {
     const token = await user.generateAuthToken();
     console.log(token);
 
-    //cookies
     res.cookie('jwt', token, {
         expires: new Date(Date.now() + 15000),
         httpOnly: true
     });
 
-    // stroring data into object
     let admin = new Admin({
         username: req.body.username,
         password: req.body.password,
     });
 
-    // passing an object to insert data into database
     Admin.create(admin).then((data) => {
         res.send(`New admin is created with username ${data.username} and password ${data.password}`);
     }).catch((err) => {
